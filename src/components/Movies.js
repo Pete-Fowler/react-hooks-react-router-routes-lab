8515da1f8c107d34e3954d0b0bb9a1cc02350d52
Pete Fowler <fowler.pete@gmail.com>
2022-09-11 10:38:46 -0600

    Finish lab

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,21 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+
+  const list = movies.map(movie => 
+    <div key={movie.title}>
+      <h2>{movie.title}</h2>
+      <p>Run time: {movie.time} min.</p>
+      <ul>
+        {movie.genres.map(genre => <li key={genre}>{genre}</li>)}
+      </ul>
+    </div>
+  );
+
+  return <div>
+    <h1>Movies Page</h1>
+    {list}
+  </div>;
 }
 
 export default Movies;
